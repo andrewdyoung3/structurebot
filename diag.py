e943d690a70786c546f9cdc7885f68c416ea87ea
Andrew Young <andrew.d.young3@gmail.com>
2026-05-29 18:31:41 +1000

    chore: gitignore logs/, sessions/, ProteinMPNN, scratch files; untrack them

--- diag.py
+++
@@ -1,6 +0,0 @@
-﻿from wsl_bridge import WSLBridge
-wsl = WSLBridge()
-print('available:', wsl.is_available())
-print('check_pyrosetta:', wsl.check_pyrosetta())
-r = wsl.run_command('/home/andre/pyrosetta_env/bin/python -c "import pyrosetta; print(chr(79)+chr(75))"')
-print('direct run:', r)
